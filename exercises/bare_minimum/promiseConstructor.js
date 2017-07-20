4ef325f1d301fab24f8030bc6344078454d5b1ec
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-19 18:25:58 -0700

    Implement promisification and promise constructor

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,28 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+
+    fs.readFile(filePath, 'utf8', function(err, content) {
+      if (err) {reject(err)}
+      else {
+        var lines = content.split("\n");
+        resolve(lines[0].slice(0, lines[0].length - 1))
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    request(url, function(err, response, body) {
+      if (err) {reject(err)} 
+      else {
+        resolve(response.statusCode);
+      }
+    })
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
@@ -23,3 +39,13 @@ module.exports = {
   getStatusCodeAsync: getStatusCodeAsync,
   pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
 };
+
+
+  // request(url, function(err, response, body) {
+  //   if (err) {
+  //     callback(err, null);
+  //     console.log('You effed up the status code dawg');
+  //   } else {
+  //     return callback(null, response.statusCode);
+  //   }
+  // })
